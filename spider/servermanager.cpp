d618af4bd5758785e9876bf3757c6214d1fd87e3
bankov <bankov_dmitrii@mail.ru>
2013-11-28 21:40:17 +0400

    Added the FAMILY parameter, which specifies the addres family.

--- spider/servermanager.cpp
+++ spider/servermanager.cpp
@@ -41,7 +41,7 @@ ServerManager::ServerManager(const std::string &server) : keepalive_(0) {
    */
   struct addrinfo hints;
   memset(&hints, 0, sizeof hints);
-  hints.ai_family = AF_UNSPEC;
+  hints.ai_family = FAMILY;
   hints.ai_socktype = SOCK_DGRAM;
 
   struct addrinfo *servinfo;
